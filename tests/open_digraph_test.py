8c473539f1ab3661ecb3af53437edced61944f94
Sukyn <colin.blake@u-psud.fr>
2021-01-26 16:08:55 +0100

    init tests opendigraph

--- tests/open_digraph_test.py
+++ tests/open_digraph_test.py
@@ -12,6 +12,14 @@ class InitTest(unittest.TestCase):
         self.assertEqual(n0.parents, [])
         self.assertEqual(n0.children, [1])
         self.assertIsInstance(n0, node)
+    def test_init_open_digraph(self):
+        n0list = [node(i, '{}'.format(i), [], [1]) for i in range(5)]
+        g = open_digraph([1], [2], n0list)
+        self.assertEqual(g.nodes[0], n0list[0])
+        self.assertEqual(g.inputs, [1])
+        self.assertEqual(g.outputs, [2])
+        self.assertIsInstance(g, open_digraph)
+        self.assertIsInstance(n0list[0], node)
 
 if __name__ == '__main__':  # the following code is called only when
     unittest.main()         # precisely this file is run
